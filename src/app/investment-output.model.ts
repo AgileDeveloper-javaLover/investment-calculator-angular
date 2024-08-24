a0dbd2877fcfc530f851eb92970fbf46fcf18e5a
AgileDeveloper-javaLover <nishantprajapati999@gmail.com>
2024-08-25 01:31:26 +0530

    Updated project

---
+++ src/app/investment-output.model.ts
@@ -0,0 +1,9 @@
+export interface InvestmentOutputData{
+        year: number,
+        interest: number,
+        valueEndOfYear: number,
+        annualInvestment: number,
+        totalInterest: number,
+        totalAmountInvested: number,
+    
+}
